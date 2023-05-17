1da2eeba73a96de9966e9b37a8c83333826927df
ccsuyzc <3338319245@qq.com>
2023-05-17 18:29:49 +0800

    面板组件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 <template>
     <div class="container">
@@ -13,4 +14,15 @@ import homeProduct from './components/HomeProduct.vue'
         <HomeNew />
         <HomeHot />
         <homeProduct />
+        <!-- 测试面板组件 -->
+        <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+            <div>
+                插槽内容
+            </div>
+        </HomePanel>
+        <HomePanel title="人气推荐" sub-title="人气推荐 杨志超帅比">
+            <div>
+                插槽内容
+            </div>
+        </HomePanel>
 </template>
